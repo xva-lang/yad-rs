b21432bd97d053804ae1c6e8a40cb081c71eb095
Dylan Giles <dylanygiles@gmail.com>
2023-12-28 16:39:52 +1100

    config

--- src/config.rs
+++ src/config.rs
@@ -2,7 +2,7 @@ use std::{
     error::Error,
     fs::File,
     io::{self, Read},
-    net::Ipv4Addr,
+    net::{Ipv4Addr, SocketAddrV4, ToSocketAddrs},
 };
 
 use serde::Deserialize;
@@ -12,13 +12,53 @@ pub(crate) struct Config {
     server: Option<ServerConfig>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub(crate) struct ServerConfig {
     host: Option<Ipv4Addr>,
     port: Option<u16>,
     ssl: Option<bool>,
 }
 
+const DEFAULT_HOST: &str = "127.0.0.1";
+const DEFAULT_PORT: u16 = 8000;
+
+impl ServerConfig {
+    pub(crate) fn get_addr(&self) -> SocketAddrV4 {
+        let (ip, port) = (
+            if let Some(h) = self.host {
+                h
+            } else {
+                DEFAULT_HOST.parse().unwrap()
+            },
+            if let Some(p) = self.port {
+                p
+            } else {
+                DEFAULT_PORT
+            },
+        );
+
+        SocketAddrV4::new(ip, port)
+    }
+}
+
+impl Default for ServerConfig {
+    fn default() -> Self {
+        Self {
+            host: Some(DEFAULT_HOST.parse().unwrap()),
+            port: Some(DEFAULT_PORT),
+            ssl: Some(false),
+        }
+    }
+}
+
+impl Config {
+    pub(crate) fn server(&self) -> ServerConfig {
+        match &self.server {
+            Some(s) => (*s).clone(),
+            None => ServerConfig::default(),
+        }
+    }
+}
 const DEFAULT_CONFIG_FILE_NAME: &str = "yad.toml";
 
 pub(crate) fn get_config(config_file: Option<&str>) -> Result<Config, Box<dyn Error>> {
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
-use crate::config::get_config;
+use std::{default, error::Error};
+
+use crate::config::{get_config, ServerConfig};
+use axum::{routing::get, Router};
 
 mod config;
 
-fn main() {
-    let config = get_config(None);
-    println!("{config:#?}")
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let config = match get_config(None) {
+        Ok(c) => c,
+        Err(e) => panic!(
+            "Failed to read the configuration file. Extended error: {}",
+            e
+        ),
+    };
+
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run our app with hyper, listening globally on port 3000
+    let addr = config.server().get_addr();
+    println!("Listening on {addr}");
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
 }
